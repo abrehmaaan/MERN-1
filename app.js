937fdeed4485b0b0a156c6138cbfc04f86783380
Abdul Rehman <abdulreh4am9a@gmail.com>
2023-02-11 20:11:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function test(name){
+    console.log("Hello " + name);
+}
+test("World");
+
+var logger = require('./login');
+// console.log(module);
+// console.log(logger);
+logger.log('Web Frameworks');
---
+++ login.js
@@ -0,0 +1,9 @@
+var url = "Http://google.com";
+function log(message){
+    console.log(message);
+}
+
+module.exports.log = log;
+//first log is endpoint i.e the name in other file second log is name in this file
+
+// module.exports.url = url;
